d042938960baa9ad3117354c0a0e00e5d8cfb8c7
Md-Alqma <mdalqma7@gmail.com>
2025-01-16 12:59:15 +0530

    created post request to create todo

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 
 let PORT = process.env.PORT || 3000;
 
+let todos = [];
+
+function generateRandomId() {
+  return Math.floor(Math.random() * 1000 + 1);
+}
+
 app.get("/", (req, res) => {
   res.send("Welcome to Todo App");
+});
+
+app.get("/todos", (req, res) => {
+  res.status(200).json(todos);
+});
+
+app.post("/todos", (req, res) => {
+  let todo = {
+    id: req.body.id,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  todos.push(todo);
+
+  res.status(201).json(todo);
 });
 
 app.listen(PORT, () => {
